36a64c88f50800ca1c679ae9b21a2882ee279a94
Durgesh Pandey <41140945+durgeshpd@users.noreply.github.com>
2025-03-01 22:11:04 +0530

    Update 56-merge-intervals.js

--- 56-merge-intervals.js
+++ 56-merge-intervals.js
@@ -2,11 +2,14 @@
 // Optimize Approch (O(n))
 
 function merge(intervals) {
+
     if (intervals.length == 0) return [];
 
+    // Step 1: Sort intervals based on the start time
     intervals.sort((a, b) => a[0] - b[0]);
     let result = [intervals[0]];
 
+    // Step 2: Iterate and Merge
     for (let i = 1; i < intervals.length; i++) {
 
         let last = result[result.length - 1];
