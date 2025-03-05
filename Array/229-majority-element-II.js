19193ad0fdc09cd0e5eaa862a5b1a51b6a7b73c2
Durgesh Pandey <41140945+durgeshpd@users.noreply.github.com>
2025-03-05 09:08:44 +0530

    Update 229-majority-element-II.js

--- Array/229-majority-element-II.js
+++ Array/229-majority-element-II.js
@@ -1,4 +1,4 @@
-
+/* 
 // Approch 1: using hashmap Time and Space Complexity is (O(n))
 
 function majorityElement(nums) {
@@ -17,6 +17,45 @@ function majorityElement(nums) {
     }
     return result;
 }
+ */
+// Optimized Approch Boyer-Moore Voting Algorithm T.C -> (O(n)) S.C (O(1))
 
-let nums = [3, 2, 3];
+function majorityElement(nums) {
+    let candidate1 = null, candidate2 = null;
+    let count1 = 0, count2 = 0;
+
+    // Step 1: Find candidates
+    for (let num of nums) {
+        if (num === candidate1) count1++;
+        else if (num === candidate2) count2++;
+        else if (count1 === 0) {
+            candidate1 = num;
+            count1 = 1;
+        } 
+        else if (count2 === 0) {
+            candidate2 = num;
+            count2 = 1;
+        }
+        else {
+            count1--;
+            count2--;
+        }
+    }
+
+    // Step 2 : Verify the candidates
+    count1 = 0, count2 = 0;
+
+    for (let num of nums) {
+        if (num === candidate1) count1++;
+        if (num === candidate2) count2++;
+    }
+
+    let result = [];
+    if (count1 > Math.floor(nums.length / 3)) result.push(candidate1);
+    if (candidate1 !== candidate2 && count2 > Math.floor(nums.length / 3)) result.push(candidate2);
+
+    return result;
+}
+
+let nums = [1, 1, 1, 3, 3, 2, 2, 2];
 console.log(majorityElement(nums));
