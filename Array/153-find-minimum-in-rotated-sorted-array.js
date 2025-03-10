fd01ac517867590188111877137ffcff74784ae6
Durgesh Pandey <41140945+durgeshpd@users.noreply.github.com>
2025-03-10 22:27:18 +0530

    Update 153-find-minimum-in-rotated-sorted-array.js

--- Array/153-find-minimum-in-rotated-sorted-array.js
+++ Array/153-find-minimum-in-rotated-sorted-array.js
@@ -7,12 +7,11 @@ function findMin(nums) {
  */
 // Optimize Approch Binary search code (O(log n))
 
-
 function findMin(nums) {
     let left = 0, right = nums.length - 1;
 
     while (left < right) {
-        let mid = (left + right) / 2;
+        let mid = Math.floor((left + right) / 2);
 
         if (nums[mid] > nums[right]) {
             left = mid + 1;
